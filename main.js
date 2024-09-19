3a0b481c67d9c6a01c9502f085cfb99642fe6ebe
Yehuda Grajower <yehudag@Yehudas-MacBook-Pro.local>
2024-09-19 15:36:11 +0300

    finished input of stats and their display on the table

--- main.js
+++ main.js
@@ -13,6 +13,8 @@ const selectPosition = document.getElementById("position-search");
 const pointsRange = document.getElementById("points-range");
 const fgRange = document.getElementById("fg-range");
 const threePercentRange = document.getElementById("3p-range");
+const tableBody = document.getElementById("table-body");
+let allPlayers = [];
 const baseUrl = "https://nbaserver-q21u.onrender.com/api/filter";
 function findPlayers(stats) {
     return __awaiter(this, void 0, void 0, function* () {
@@ -24,8 +26,9 @@ function findPlayers(stats) {
                 },
                 body: JSON.stringify(stats)
             });
-            const players = response.json();
+            const players = yield response.json();
             console.log(players);
+            yield addPlayersToTable(players);
             return players;
         }
         catch (error) {
@@ -34,12 +37,55 @@ function findPlayers(stats) {
         }
     });
 }
-function makeRequest() {
-    const newStat = {
-        position: selectPosition === null || selectPosition === void 0 ? void 0 : selectPosition.innerText,
-        points: +pointsRange.innerText,
-        twoPercent: +fgRange.innerText,
-        threePercent: +threePercentRange.innerText
-    };
-    return newStat;
+function makeRequest(event) {
+    return __awaiter(this, void 0, void 0, function* () {
+        event.preventDefault();
+        const newStat = {
+            position: selectPosition === null || selectPosition === void 0 ? void 0 : selectPosition.value,
+            points: +pointsRange.value,
+            twoPercent: +fgRange.value,
+            threePercent: +threePercentRange.value
+        };
+        yield findPlayers(newStat);
+        return newStat;
+    });
 }
+function addPlayersToTable(players) {
+    players.forEach(player => {
+        createRow(player);
+    });
+}
+function createRow(player) {
+    const newRow = document.createElement("tr");
+    const playerName = document.createElement("td");
+    playerName.innerText = player.playerName;
+    const position = document.createElement("td");
+    position.innerText = player.position;
+    const points = document.createElement("td");
+    points.innerText = player.points.toString();
+    const twoPercent = document.createElement("td");
+    twoPercent.innerText = player.twoPercent.toString();
+    const threePercent = document.createElement("td");
+    threePercent.innerText = player.threePercent.toString();
+    const action = document.createElement("td");
+    const button = document.createElement("input");
+    button.type = "submit";
+    button.id = "add-player-button";
+    button.value = `Add ${player.playerName} to Current Team`;
+    action.appendChild(button);
+    newRow.appendChild(playerName);
+    newRow.appendChild(position);
+    newRow.appendChild(points);
+    newRow.appendChild(twoPercent);
+    newRow.appendChild(threePercent);
+    newRow.appendChild(action);
+    tableBody.appendChild(newRow);
+}
+// async function renderPage() {
+//     tableBody.innerText = '';
+//     allPlayers = await findPlayers();
+//     if(allPlayers.length > 0){
+//         dispalyPlayers(allPlayers);
+//     }
+// }
+statsForm.addEventListener("submit", makeRequest);
--- main.ts
+++ main.ts
@@ -1,28 +1,31 @@
 
 interface Stats{
-    position?: string,
-    points?: number
+    position: string,
+    points: number,
     twoPercent: number,
-    threePercent: number,
+    threePercent: number
 }
 
 interface Players{
     position: string,
+    playerName: string
     points: number,
     twoPercent: number,
     threePercent: number,
-    playerName: string
 }
 
-const statsForm = document.getElementById("stats-form");
-const selectPosition = document.getElementById("position-search"); 
-const pointsRange = document.getElementById("points-range");
-const fgRange = document.getElementById("fg-range");
-const threePercentRange = document.getElementById("3p-range");
+const statsForm = document.getElementById("stats-form") as HTMLFormElement;
+const selectPosition = document.getElementById("position-search")as HTMLSelectElement;
+const pointsRange = document.getElementById("points-range") as HTMLInputElement;
+const fgRange = document.getElementById("fg-range") as HTMLInputElement;
+const threePercentRange = document.getElementById("3p-range") as HTMLInputElement;
+const tableBody = document.getElementById("table-body") as HTMLBodyElement;
+
+let allPlayers: Players[] = [];
 
 const baseUrl: string = "https://nbaserver-q21u.onrender.com/api/filter";
 
-async function findPlayers(stats: Stats): Promise<Players[]>{
+async function findPlayers(stats: Stats) : Promise<Players[]>{
     try{
         const response = await fetch(`${baseUrl}`, {
             method: "POST",
@@ -31,8 +34,9 @@ async function findPlayers(stats: Stats): Promise<Players[]>{
             },
             body: JSON.stringify(stats)
         });
-        const players = response.json();
+        const players = await response.json();
         console.log(players);
+        await addPlayersToTable(players);
         return players
     }
     catch(error){
@@ -41,13 +45,67 @@ async function findPlayers(stats: Stats): Promise<Players[]>{
     }
 }
 
-function makeRequest(): Stats{
+async function makeRequest(event:Event): Promise<Stats>{
+    event.preventDefault();
     const newStat:Stats = {
-        position: selectPosition?.innerText,
-        points: +pointsRange.innerText,
-        twoPercent: +fgRange.innerText,
-        threePercent: +threePercentRange.innerText
+        position: selectPosition?.value,
+        points: +pointsRange.value,
+        twoPercent: +fgRange.value,
+        threePercent: +threePercentRange.value
     }
-
+    await findPlayers(newStat);
     return newStat
 }
+
+function addPlayersToTable(players:Players[]){
+    players.forEach(player => {
+        createRow(player);
+    });
+}
+
+function createRow(player: Players){
+    const newRow = document.createElement("tr");
+
+    const playerName = document.createElement("td");
+    playerName.innerText = player.playerName; 
+
+    const position = document.createElement("td");
+    position.innerText = player.position; 
+
+    const points = document.createElement("td");
+    points.innerText = player.points.toString(); 
+
+    const twoPercent = document.createElement("td");
+    twoPercent.innerText = player.twoPercent.toString(); 
+
+    const threePercent = document.createElement("td");
+    threePercent.innerText = player.threePercent.toString();
+
+    const action = document.createElement("td");
+    const button = document.createElement("input");
+    button.type = "submit";
+    button.id = "add-player-button";
+    button.value = `Add ${player.playerName} to Current Team`;
+    
+    action.appendChild(button);
+    newRow.appendChild(playerName);
+    newRow.appendChild(position);
+    newRow.appendChild(points);
+    newRow.appendChild(twoPercent);
+    newRow.appendChild(threePercent);
+    newRow.appendChild(action)
+    tableBody.appendChild(newRow);
+}
+
+
+
+// async function renderPage() {
+//     tableBody.innerText = '';
+//     allPlayers = await findPlayers();
+//     if(allPlayers.length > 0){
+//         dispalyPlayers(allPlayers);
+//     }
+// }
+
+
+statsForm.addEventListener("submit", makeRequest);
